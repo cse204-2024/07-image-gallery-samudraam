7cf9665fd23a253c239932b9f960016f2d17612c
samudraam <112435495+samudraam@users.noreply.github.com>
2024-03-26 00:24:09 -0500

    Added three more images and semantic htmlhtml
    articles, headers, ect

--- script.js
+++ script.js
@@ -2,25 +2,26 @@
 const images = [
   "https://images.unsplash.com/photo-1519608487953-e999c86e7455?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //1
   "https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?q=80&w=2755&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //2
-  "https://images.unsplash.com/photo-1560583306-bd304a162229?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGN5YmVycHVua3xlbnwwfDB8MHx8fDA%3D", //3
-  "https://images.unsplash.com/photo-1557515126-1bf9ada5cb93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D", //4
-  "https://plus.unsplash.com/premium_photo-1690576974191-e9a3cb22591e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fHw%3D", //5
-  "https://images.unsplash.com/photo-1571331154805-c2a013108395?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D", //6
-  "https://images.unsplash.com/Ys-DBJeX0nE.JPG?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D", //7
+  "https://images.unsplash.com/photo-1560583306-bd304a162229?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //3
+  "https://images.unsplash.com/photo-1557515126-1bf9ada5cb93?q=80&w=2531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //4
+  "https://images.unsplash.com/photo-1502872364588-894d7d6ddfab?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //5
+  "https://images.unsplash.com/photo-1571331154805-c2a013108395?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //6
+  "https://images.unsplash.com/Ys-DBJeX0nE.JPG?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //7
   "https://images.unsplash.com/photo-1555397430-57791c75748a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //8
-  "https://images.unsplash.com/photo-1552668355-4f6d28b9dc7e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTR8ODMxMTc4OXx8ZW58MHx8fHx8", //9
-  "https://images.unsplash.com/photo-1533577116850-9cc66cad8a9b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8bjFrVkI0cEprUDh8fGVufDB8fHx8fA%3D%3D", //10
-  "https://images.unsplash.com/photo-1587398458654-6a55d2d0dd61?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8NTc0MzMxfHxlbnwwfHx8fHw%3D", //11
-  "https://images.unsplash.com/photo-1534214526114-0ea4d47b04f2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3xwY3F1OGNxaG1pRXx8ZW58MHx8fHx8", //12
+  "https://images.unsplash.com/photo-1552668355-4f6d28b9dc7e?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //9
+  "https://images.unsplash.com/photo-1533577116850-9cc66cad8a9b?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //10
+  "https://images.unsplash.com/photo-1578608738964-cd27acd5af2c?q=80&w=2302&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //11
+  "https://images.unsplash.com/photo-1534214526114-0ea4d47b04f2?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", //12
+  "https://images.unsplash.com/photo-1619286875682-1aed54a4bade?q=80&w=2461&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1485001564903-56e6a54d46ef?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1555852224-2a3e675fc47e?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
-console.log(images.length);
 //Function Call
 initialize();
 function initialize() {
   var imgGal = document.getElementById("image-gal");
   for (i = 0; i < images.length; i++) {
-    let articleElement = document.createElement("article");
     let imageElement = document.createElement("img");
     imageElement.src = images[i];
     imageElement.width = 400;
@@ -174,6 +175,7 @@ function prevImg(id) {
   displayImage(nI);
 }
 
+// click fn for all elms (is there a way to do this iterativley?)
 document.getElementById("img0").addEventListener("click", function () {
   displayImage("img0");
 });
@@ -186,4 +188,50 @@ document.getElementById("img2").addEventListener("click", function () {
   displayImage("img2");
 });
 
+document.getElementById("img3").addEventListener("click", function () {
+  displayImage("img3");
+});
 
+document.getElementById("img4").addEventListener("click", function () {
+  displayImage("img4");
+});
+
+document.getElementById("img5").addEventListener("click", function () {
+  displayImage("img5");
+});
+
+document.getElementById("img6").addEventListener("click", function () {
+  displayImage("img6");
+});
+
+document.getElementById("img7").addEventListener("click", function () {
+  displayImage("img7");
+});
+
+document.getElementById("img8").addEventListener("click", function () {
+  displayImage("img8");
+});
+
+document.getElementById("img9").addEventListener("click", function () {
+  displayImage("img9");
+});
+
+document.getElementById("img10").addEventListener("click", function () {
+  displayImage("img10");
+});
+
+document.getElementById("img11").addEventListener("click", function () {
+  displayImage("img11");
+});
+
+document.getElementById("img12").addEventListener("click", function () {
+  displayImage("img12");
+});
+
+document.getElementById("img13").addEventListener("click", function () {
+  displayImage("img13");
+});
+
+document.getElementById("img14").addEventListener("click", function () {
+  displayImage("img14");
+});
